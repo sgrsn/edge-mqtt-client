3adbeebbcecd53b75e2105c46c886b40b3d24c4d
sgrsn <sgrsn1711@gmail.com>
2024-12-18 18:43:33 +0900

    ⚡️ Watchdog stop motor control timer

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,11 @@ const uint32_t MONITORING_RATE_US = 1000000;
 const uint32_t MQTT_RATE_US = 10000;
 const uint32_t CONTROL_RATE_US = 50000;
 
+esp_timer_handle_t timer_watchdog;
+esp_timer_handle_t timer_monitoring;
+esp_timer_handle_t timer_mqtt_loop;
+esp_timer_handle_t timer_control;
+
 static void HeartbeatTimer()
 {
   std::string heartbeat = std::to_string(millis());
@@ -25,18 +30,26 @@ static void HeartbeatTimer()
 static void MonitoringTimer()
 {
   static std::string heartbeat;
+  static bool stop = false;
   mqtt.getLastValue("esp/watchdog/heartbeat", heartbeat);
   unsigned long last_heartbeat = 0;
   if (!heartbeat.empty())
     last_heartbeat = std::stoi(heartbeat);
   unsigned long error_time = millis() - last_heartbeat;
-  debug.println("error_time: ", error_time);
 
-  // 4回以上連続で受信がない場合は停止
+  // 4回以上連続で受信がない場合モーターを停止
   if (error_time > WATCHDOG_RATE_US * 4) {
+    debug.println("(MonitoringTimer) ", "error_time: ", error_time);
+    esp_timer_stop(timer_control);
     servo1.setPosition(0);
     servo2.setPosition(0);
     esc.stop();
+    stop = true;
+  }
+  else if (stop) {
+    debug.println("(MonitoringTimer) ", "restart control timer");
+    esp_timer_start_periodic(timer_control, CONTROL_RATE_US);
+    stop = false;
   }
 }
 
@@ -58,7 +71,7 @@ static void Control()
   servo2.setPosition(y);
 
   if (startStop) {
-    esc.linearAccelSpeed(slider, 0.1, 20);
+    esc.linearAccelSpeed(slider, 1.0, 20);
   } else {
     esc.stop();
   }
@@ -66,40 +79,37 @@ static void Control()
 
 void initializeTimer()
 {
-  esp_timer_handle_t testTimer;
   esp_timer_create_args_t timerConfig;
   timerConfig.callback = reinterpret_cast<esp_timer_cb_t>(HeartbeatTimer);
   timerConfig.dispatch_method = ESP_TIMER_TASK;
   timerConfig.name = "MainTimer";
-  esp_timer_create(&timerConfig, &testTimer);
-  esp_timer_start_periodic(testTimer, WATCHDOG_RATE_US);
+  esp_timer_create(&timerConfig, &timer_watchdog);
+  esp_timer_start_periodic(timer_watchdog, WATCHDOG_RATE_US);
 
-  esp_timer_handle_t testTimer2;
   esp_timer_create_args_t timerConfig2;
   timerConfig2.callback = reinterpret_cast<esp_timer_cb_t>(MonitoringTimer);
   timerConfig2.dispatch_method = ESP_TIMER_TASK;
   timerConfig2.name = "MonitoringTimer";
-  esp_timer_create(&timerConfig2, &testTimer2);
-  esp_timer_start_periodic(testTimer2, MONITORING_RATE_US);
+  esp_timer_create(&timerConfig2, &timer_monitoring);
+  esp_timer_start_periodic(timer_monitoring, MONITORING_RATE_US);
 
-  esp_timer_handle_t testTimer3;
   esp_timer_create_args_t timerConfig3;
   timerConfig3.callback = reinterpret_cast<esp_timer_cb_t>(MqttLoop);
   timerConfig3.dispatch_method = ESP_TIMER_TASK;
   timerConfig3.name = "MqttTimer";
-  esp_timer_create(&timerConfig3, &testTimer3);
-  esp_timer_start_periodic(testTimer3, MQTT_RATE_US);
+  esp_timer_create(&timerConfig3, &timer_mqtt_loop);
+  esp_timer_start_periodic(timer_mqtt_loop, MQTT_RATE_US);
   
-  esp_timer_handle_t testTimer4;
   esp_timer_create_args_t timerConfig4;
   timerConfig4.callback = reinterpret_cast<esp_timer_cb_t>(Control);
   timerConfig4.dispatch_method = ESP_TIMER_TASK;
   timerConfig4.name = "ControlTimer";
-  esp_timer_create(&timerConfig4, &testTimer4);
-  esp_timer_start_periodic(testTimer4, CONTROL_RATE_US);
+  esp_timer_create(&timerConfig4, &timer_control);
+  esp_timer_start_periodic(timer_control, CONTROL_RATE_US);
 }
 
 void setup() {
+  debug.println("(setup)", "start");
   esc.initializeTimer();
   esc.stop();
   mqtt.init();
@@ -109,6 +119,7 @@ void setup() {
   mqtt.registerTopic<double>("control/joystick/x");
   mqtt.registerTopic<double>("control/joystick/y");
   initializeTimer();
+  debug.println("(setup)", "end");
 }
 
 void loop() {
